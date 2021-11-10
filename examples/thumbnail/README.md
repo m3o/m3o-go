48a07e015accab3ef4c05661202452ba8537b681
m3o-actions <>
2021-11-10 11:32:49 +0000

    Commit from m3o/m3o action

--- examples/thumbnail/README.md
+++ examples/thumbnail/README.md
@@ -26,7 +26,7 @@ func TakeScreenshotOfAurl() {
 	thumbnailService := thumbnail.NewThumbnailService(os.Getenv("M3O_API_TOKEN"))
 	rsp, err := thumbnailService.Screenshot(&thumbnail.ScreenshotRequest{
 		Height: 600,
-Url: "https://m3o.com",
+Url: "https://google.com",
 Width: 600,
 
 	})
--- examples/thumbnail/screenshot/takeScreenshotOfAUrl/main.go
+++ examples/thumbnail/screenshot/takeScreenshotOfAUrl/main.go
@@ -12,7 +12,7 @@ func main() {
 	thumbnailService := thumbnail.NewThumbnailService(os.Getenv("M3O_API_TOKEN"))
 	rsp, err := thumbnailService.Screenshot(&thumbnail.ScreenshotRequest{
 		Height: 600,
-		Url:    "https://m3o.com",
+		Url:    "https://google.com",
 		Width:  600,
 	})
 	fmt.Println(rsp, err)
